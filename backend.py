0281ee75e1efad1c0a3ac2b5a224112bf6785cc1
elichad <eli.chadwick.256@gmail.com>
2021-04-01 11:55:36 +0000

    add backend skeleton

---
+++ backend.py
@@ -0,0 +1,43 @@
+# Dictionary of questions
+# Class(?)es(?) for 
+# body of get_question()
+# body of put_answer()
+# 
+
+Class Question():
+	def __init__(self, text, options):
+		self.text = text
+		self.options = options
+
+	
+
+question_returned = ("question text", question[1].keys())
+
+# example data structure containing questions
+# format is {id: question}
+example={
+  1: Question("example text",  {"option 1 ": 2, "option 2": 3}),
+  :2 }
+
+
+
+            
+
+def get_question():
+    """Returns:
+    the text of a question,
+    an iterable of options, and
+    the next steps for each option."""
+    #question_returned (as above)
+    #status = boolean -if False get_badge-
+    return (status, question_returned)
+    
+def put_answer(question_text, chosen_option):
+    """ Returns status:
+    Error, etc.
+    More questions True/False"""
+    #chosen option = string
+    return status
+    
+def get_badge():
+    return md_code
